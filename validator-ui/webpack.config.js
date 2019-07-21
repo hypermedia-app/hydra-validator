33d939c46d64f2793eef23bf5c37517ab5e4f578
tpluscode <tpluscode@users.noreply.github.com>
2019-07-21 14:12:20 +0200

    style: apply eslint

--- validator-ui/webpack.config.js
+++ validator-ui/webpack.config.js
@@ -1,13 +1,13 @@
-const { resolve } = require('path');
-const merge = require('webpack-merge');
-const createDefaultConfig = require('@open-wc/building-webpack/modern-config');
+const { resolve } = require('path')
+const merge = require('webpack-merge')
+const createDefaultConfig = require('@open-wc/building-webpack/modern-config')
 
 // If you don't need IE11 support, use the modern-config instead
 // import createDefaultConfig from '@open-wc/building-webpack/modern-config';
 
 const config = createDefaultConfig({
-  input: resolve(__dirname, './index.html'),
-});
+  input: resolve(__dirname, './index.html')
+})
 
 module.exports = merge(config, {
   module: {
@@ -22,4 +22,4 @@ module.exports = merge(config, {
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ]
   }
-});
+})
