368d47cb57aeb2a33f27bdf582a788f30b13e8ca
tpluscode <tpluscode@users.noreply.github.com>
2019-07-22 09:36:45 +0200

    chore: adjust jest settings

--- validator-core/jest.config.js
+++ validator-core/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    roots: ['<rootDir>/src'],
+    roots: ['<rootDir>'],
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.ts'],
+    collectCoverageFrom: ['**/*.ts'],
 }
