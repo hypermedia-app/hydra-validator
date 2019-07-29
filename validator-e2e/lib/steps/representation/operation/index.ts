01e6bcb4a1847a11a7cf87b76f5e493ceaf1a095
tpluscode <tpluscode@users.noreply.github.com>
2019-07-29 21:52:41 +0200

    fix: silently ignore non-strict operation blocks

--- validator-e2e/lib/steps/representation/operation/index.ts
+++ validator-e2e/lib/steps/representation/operation/index.ts
@@ -28,9 +28,14 @@ export class OperationStep extends ScenarioStep {
             const operation = resource.operations.find(op => op.supportedOperation.id === step.operationId || op.supportedOperation.types.contains(step.operationId))
             if (!operation) {
                 const message = `Operation ${step.operationId} not found`
-                return {
-                    result: Result.Failure(message),
+                if (step.strict) {
+                    return {
+                        result: Result.Failure(message),
+                    }
                 }
+
+                // TODO: add a 'debug' result for diagnostic purposes
+                return {}
             }
 
             if (step.executed) {
